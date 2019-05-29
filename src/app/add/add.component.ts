7236088a148b546ae8b3ca52310c75dc312ccc0b
amchavan <amchavan>
2019-05-29 17:54:55 +0200

    Updated

--- src/app/add/add.component.ts
+++ src/app/add/add.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-add',
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit() {
   }
 
+
+  public open() {
+    console.log( ">>> open() called" )
+    // const config = new TemplateModalConfig<IContext, string, string>(this.modalDialog);
+    //
+    // config.closeResult = "closed!";
+    // config.context = { data: "" };
+    //
+    // this.modalService
+    //     .open(config)
+    //     .onApprove(result => {})
+    //     .onDeny(result => {});
+  }
 }
