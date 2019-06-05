a810d8a04cb533b175aa72c75478a52f925db983
amchavan <amchavan>
2019-06-05 11:24:47 +0200

    Added alerts

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookingComponent implements OnInit {
 
+  isVisibleWarning = true;
+  isVisibleInfo = true;
+
   constructor() { }
 
   ngOnInit() {
