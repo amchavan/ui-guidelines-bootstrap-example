c6e145b73c8165979bbfa52e55d16c2d0b1b8c76
amchavan <amchavan>
2019-06-26 12:24:03 +0200

    Added dirty checking

--- src/app/form-validation-example/form-validation-example.component.ts
+++ src/app/form-validation-example/form-validation-example.component.ts
@@ -1,48 +1,63 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  selector: 'app-form-validation-example',
-  templateUrl: './form-validation-example.component.html',
-  styleUrls: ['./form-validation-example.component.css']
+    selector: 'app-form-validation-example',
+    templateUrl: './form-validation-example.component.html',
+    styleUrls: ['./form-validation-example.component.css']
 })
 
 export class FormValidationExampleComponent implements OnInit {
-  regForm: FormGroup;
-  submitted = false;
+    regForm: FormGroup;
+    submitted = false;
+    isDirty = false;
+    defaultEmail = 'user@example.com';
+    defaultPassword = '123456';
+    dafaultFormValue: any;
 
-  constructor( fb: FormBuilder,  public activeModal: NgbActiveModal ) {
-    this.regForm = fb.group({
-      email:    ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-    });
-  }
+    constructor(fb: FormBuilder, public activeModal: NgbActiveModal) {
+        this.regForm = fb.group({
+            email:    [ this.defaultEmail,    [Validators.required, Validators.email]],
+            password: [ this.defaultPassword, [Validators.required, Validators.minLength(6)]],
+        });
 
-  // convenience getter for easy access to form fields
-  get f() {
-    return this.regForm.controls;
-  }
+        this.regForm.valueChanges.subscribe(val => {
+            this.isDirty = true;
+        });
 
-  ngOnInit() {
-  }
-
-  onSubmit(): void {
-    this.submitted = true;
-
-    // stop here if form is invalid
-    if (this.regForm.invalid) {
-      return;
+        this.dafaultFormValue = this.regForm.value;
     }
 
-    // Now "submit" the form
-    const msg = JSON.stringify( this.regForm.value, null, 4 );
-    console.log( msg );
-    alert( msg );
-  }
+    // convenience getter for easy access to form fields
+    get f() {
+        return this.regForm.controls;
+    }
 
-  onReset(): void {
-    this.submitted = false;
-    this.regForm.reset();
-  }
+    ngOnInit() {
+    }
+
+    onSubmit(): void {
+        this.submitted = true;
+
+        // stop here if form is invalid
+        if (this.regForm.invalid) {
+            return;
+        }
+
+        // Now "submit" the form
+        this.isDirty = false;
+        const msg = JSON.stringify(this.regForm.value, null, 4);
+        console.log(msg);
+        alert(msg);
+    }
+
+    onReset(): void {
+        this.isDirty = false;
+        this.submitted = false;
+        this.regForm.reset( {
+            email:     this.defaultEmail,
+            password:  this.defaultPassword
+        });
+    }
 }
