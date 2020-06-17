e9a94424379bc3d1038803246b33e25ef8a9e746
amchavan <amchavan>
2020-06-17 11:30:53 +0200

    First shot at integrating busy indicator as separate library

---
+++ projects/busy-indicator/src/lib/busy-indicator.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+
+@Injectable()
+export class BusyIndicatorService {
+    private subject = new Subject<boolean>();
+
+    show( option: boolean ) {
+        this.subject.next( option );
+    }
+
+    subscription(): Observable<any> {
+        return this.subject.asObservable();
+    }
+}
---
+++ projects/busy-indicator/src/projects.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of busy-indicator
+ */
+
+export * from './lib/busy-indicator.component';
+export * from './lib/busy-indicator.module';
+export * from './lib/busy-indicator.service';
